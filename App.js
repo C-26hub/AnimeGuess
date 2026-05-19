81b289fdc4ab42bb69fdeec081b11fd2fe8990fb
Maria Cecília <cecilmari33@gmail.com>
2026-05-19 14:59:57 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { NavigationContainer }
+from "@react-navigation/native";
+
+import { createNativeStackNavigator }
+from "@react-navigation/native-stack";
+
+import HomeScreen
+from "./src/screens/HomeScreen";
+
+import GameScreen
+from "./src/screens/GameScreen";
+
+const Stack =
+createNativeStackNavigator();
+
+export default function App() {
+
+  return (
+
+    <NavigationContainer>
+
+      <Stack.Navigator>
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+
+        <Stack.Screen
+          name="Game"
+          component={GameScreen}
+        />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  );
+}
